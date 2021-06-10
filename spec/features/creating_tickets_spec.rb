2ff1d9c5cc036c5223f54599ad64644dd4806937
manami-d <manami.davidson@hotmail.com>
2021-06-10 15:09:15 +1000

    Add Devise + sign up feature

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -20,8 +20,8 @@ RSpec.feature "Users can create new tickets" do
     click_button "Create Ticket"
 
     expect(page).to have_content "Ticket has not been created."
-    expect(page).to have_content "Name can't be blank"
-    expect(page).to have_content "Description can't be blank"
+    expect(page).to have_content "can't be blank"
+    expect(page).to have_content "can't be blank"
   end
 
   scenario "with an invalid description" do
@@ -30,6 +30,6 @@ RSpec.feature "Users can create new tickets" do
     click_button "Create Ticket"
 
     expect(page).to have_content "Ticket has not been created."
-    expect(page).to have_content "Description is too short"
+    expect(page).to have_content "is too short"
   end
 end
