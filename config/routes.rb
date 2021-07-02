7a7255f7dc787ad9487da9417335e7ae071cb587
manami-d <manami.davidson@hotmail.com>
2021-07-02 10:17:21 +1000

    Only admins can create, edit, or delete projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   namespace :admin do
     root 'application#index'
+
+    resources :projects, except: [:index, :show]
   end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "projects#index"
 
-  resources :projects do
+  resources :projects, only: [:index, :show] do
     resources :tickets
   end
 end
